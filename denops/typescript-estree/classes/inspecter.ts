ed4dca53008227e87365db66bf0b669e3b05ce28
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-03-31 12:08:39 +0900

    refactor: denops依存していない処理はlibへ

--- denops/typescript-estree/classes/inspecter.ts
+++ denops/typescript-estree/classes/inspecter.ts
@@ -1,9 +1,8 @@
 import { Denops } from "jsr:@denops/std";
 import * as fn from "jsr:@denops/std/function";
 
-import { TSESTree } from "npm:@typescript-eslint/typescript-estree";
-
 import { getCurrentBufAst, getCurrentBufCode } from "../lib/utils.ts";
+import { findNodesAtPosition } from "../lib/ast.ts";
 
 export default class Inspecter {
   #denops: Denops;
@@ -19,61 +18,12 @@ export default class Inspecter {
     return pos;
   };
 
-  #findAstNodeAtPosition = async (pos: number) => {
-    const ast = await getCurrentBufAst(this.#denops);
-    if (!ast) return [];
-
-    const nodes = this.#findNodesAtPosition(ast, pos);
-    return nodes;
-  };
-
-  #findNodesAtPosition = (ast: TSESTree.Program, position: number) => {
-    const nodes: TSESTree.Node[] = [];
-
-    const traverse = (node: TSESTree.Node) => {
-      if (!node.range) return;
-      nodes.push(node);
-
-      for (const key in node) {
-        const child = node[key as keyof typeof node];
-        if (Array.isArray(child)) {
-          for (const subChild of child) {
-            if (
-              subChild &&
-              typeof subChild === "object" &&
-              "range" in subChild
-            ) {
-              traverse(subChild);
-            }
-          }
-        } else if (child && typeof child === "object" && "range" in child) {
-          traverse(child);
-        }
-      }
-    };
-
-    traverse(ast);
-
-    return nodes
-      .map((node) => {
-        const [start, end] = node.range;
-        return {
-          type: node.type,
-          start,
-          end,
-        };
-      })
-      .filter(({ start, end }) => {
-        return start <= position && position <= end;
-      })
-      .toSorted((a, b) => {
-        return a.start === b.start ? a.end - b.end : a.start - b.start;
-      });
-  };
-
   inspect = async () => {
+    const ast = await getCurrentBufAst(this.#denops);
+    if (!ast) return;
+
     const pos = await this.#getCursorPosition();
-    const matchingNodes = await this.#findAstNodeAtPosition(pos);
+    const matchingNodes = findNodesAtPosition(ast, pos);
     console.log(matchingNodes);
   };
 }
--- denops/typescript-estree/lib/ast.ts
+++ denops/typescript-estree/lib/ast.ts
@@ -8,5 +8,52 @@ export const parseToAst = (code: string) => {
     return null;
   }
 };
+
 export const getMatchingNodes = (ast: TSESTree.Program, selector: string) =>
   esquery(ast, selector);
+
+export const findNodesAtPosition = (
+  ast: TSESTree.Program,
+  position: number,
+) => {
+  const nodes: TSESTree.Node[] = [];
+
+  const traverse = (node: TSESTree.Node) => {
+    if (!node.range) return;
+    nodes.push(node);
+
+    for (const key in node) {
+      const child = node[key as keyof typeof node];
+      if (Array.isArray(child)) {
+        for (const subChild of child) {
+          if (subChild && typeof subChild === "object" && "range" in subChild) {
+            traverse(subChild);
+          }
+        }
+      } else if (child && typeof child === "object" && "range" in child) {
+        traverse(child);
+      }
+    }
+  };
+
+  traverse(ast);
+
+  return nodes
+    .map((node) => {
+      const [start, end] = node.range;
+      return {
+        type: node.type,
+        start,
+        end,
+      };
+    })
+    .filter(({ start, end }) => {
+      return start <= position && position <= end;
+    })
+    .toSorted((a, b) => {
+      if (a.start === b.start) {
+        return a.end - b.end;
+      }
+      return a.start - b.start;
+    });
+};
